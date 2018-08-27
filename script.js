840cfffeb89a40912e3d4e6b764f6c137cbb461e
Dawn Saquin <dawnsaq@gmail.com>
2018-08-27 21:54:43 +0200

    nav bar difficulties

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+//  below is mobile version nav bar
+function myFunction() {
+  if (window.innerWidth <= 700) {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+}
+
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//       x.className += " responsive";
+//   } else {
+//       x.className = "topnav";
+//   }
+
 // below scrolls navbar down
 
 const nav = document.querySelector("#main");
@@ -17,7 +37,7 @@ window.addEventListener("scroll", fixNav);
 
 // below highlights menu items in nav bar
 
-const navItems = document.querySelectorAll(".item");
+const navItems = document.querySelectorAll(".item-nav");
 
 function highlightLink() {
   this.style.background = "white";
@@ -36,19 +56,9 @@ navItems.forEach(item => item.addEventListener("mouseout", unHighlightLink));
 
 const hits = [].slice.call(document.querySelectorAll(".song"));
 const iframe = [].slice.call(document.querySelectorAll(".song iframe"));
-console.log(hits);
 let iframed = "";
 
 function toggleVideo(e) {
-  // console.log(e);
-  // for (var i = 0; i < hits.length; i++) {
-  //   if (hits[1].clicked) {
-  //     // hits1.iframe.height = 315;
-  //     console.log(hits1);
-  //   }
-  // }
-  // console.log(this);
-
   const getSelectedIndex = () => {
     for (let i = 0; i < hits.length; i++) {
       if (hits[i] === e.srcElement) return i;
@@ -59,15 +69,52 @@ function toggleVideo(e) {
   let iframed = iframe[clickedIndex];
 
   if (iframed.height == 0) {
-    iframed.height = 315;
+    iframed.height = "315";
   } else {
     iframed.height = 0;
   }
   e.preventDefault();
 }
-// }
 
-// hits.forEach(hit => hit.addEventListener("click", toggleVideo));
-for (let i = 0; i < hits.length; i++) {
-  hits[i].addEventListener("click", toggleVideo);
+hits.forEach(hit => hit.addEventListener("click", toggleVideo));
+
+//
+//
+//
+// scroll for shoppping
+const slider = document.querySelector(".shopping-items");
+const items = document.querySelectorAll(".item");
+let isDown = false; // flag variable
+let startX;
+let scrollLeft;
+
+function hoverImage() {
+  console.log(this);
 }
+
+slider.addEventListener("mousedown", e => {
+  isDown = true;
+  slider.classList.add("active");
+  // console.log(e.pageX) //gives the x coordinate of page
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", e => {
+  if (!isDown) return; // stops fn from running
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  // console.log({ x, startX });
+  const walk = (x - startX) * 3;
+  // console.log(walk);
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+items.forEach(item => item.addEventListener("mouseover", hoverImage));
